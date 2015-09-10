4bc4ec11facc9c9027a71b8c3ed3ffdc322a257a
Matthew McFarling <matt@codemancode.com>
2015-09-10 13:46:13 -0400

    sharefile file

--- lib/carrierwave/sharefile/client.rb
+++ lib/carrierwave/sharefile/client.rb
@@ -70,7 +70,7 @@ module CarrierWave
         post_body << "Content-Disposition: form-data; name=\"File1\"; filename=\"#{filename}\"#{newline}"
         post_body << "Content-Type: application/octet-stream#{newline}"
         post_body << "#{newline}"
-        post_body << File.read(tmpfile.path)
+        post_body << File.read(tmpfile.tempfile)
         post_body << "#{newline}--#{boundary}--#{newline}"
          
         request = Net::HTTP::Post.new(uri.request_uri)
--- lib/carrierwave/sharefile/version.rb
+++ lib/carrierwave/sharefile/version.rb
@@ -1,5 +1,5 @@
 module Carrierwave
   module Sharefile
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
